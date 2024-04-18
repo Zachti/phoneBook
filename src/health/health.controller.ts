42c2374973b08f884d37ede7b82c4929a8a3b1c0
Zachti <114566720+Zachti@users.noreply.github.com>
2024-04-18 19:00:53 +0300

    removed redis health check cause it cant be performed locally

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -2,11 +2,9 @@ import { Controller, Get } from '@nestjs/common';
 import {
   HealthCheck,
   HealthCheckService,
-  MicroserviceHealthIndicator,
   TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
 import { InjectDataSource } from '@nestjs/typeorm';
-import { Transport, RedisOptions } from '@nestjs/microservices';
 import { DataSource } from 'typeorm';
 
 @Controller('health')
@@ -15,24 +13,13 @@ export class HealthController {
     private health: HealthCheckService,
     private db: TypeOrmHealthIndicator,
     @InjectDataSource('mysql') private readonly mysqlDS: DataSource,
-    private microservice: MicroserviceHealthIndicator
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
     return this.health.check([
-      () => this.db.pingCheck('mysql', {connection: this.mysqlDS}),
-      async () =>
-        this.microservice.pingCheck<RedisOptions>('redis', {
-          transport: Transport.REDIS,
-          options: {
-            host: 'redis',
-            port: 6379,
-            username: 'xxxx',
-            password: 'xxxx',
-          },
-        }),
+      () => this.db.pingCheck('mysql', { connection: this.mysqlDS }),
     ]);
   }
 }
