cdaf95be01e9d57c8ccf0c71a2f63d5bb4a30cad
Zachti <114566720+Zachti@users.noreply.github.com>
2024-04-19 11:39:41 +0300

    added search dto transformer and fixed skip > count pagination bug

--- src/typeorm/seed/seed.service.ts
+++ src/typeorm/seed/seed.service.ts
@@ -41,7 +41,6 @@ export class SeedService implements OnModuleDestroy, OnModuleInit {
   }
 
   private async createTable() {
-    //await this.ds.query(`DROP TABLE contacts`);
     this.logger.debug('Creating table...');
     await this.ds.query(`CREATE TABLE IF NOT EXISTS contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
